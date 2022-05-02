e87a124d9f045b9322d6abcf456ecdb8904deb2e
Kirk Charles Niverba <kirkniverba23@gmail.com>
2022-05-03 03:58:10 +0800

    fix: app not launching

--- android/app/src/main/kotlin/com/example/mdu1_launcher/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mdu1_launcher/MainActivity.kt
@@ -1,21 +1,6 @@
 package com.example.mdu1_launcher
 
 import io.flutter.embedding.android.FlutterActivity
-import android.os.Bundle
-import android.provider.Settings
-import android.net.Uri
-import android.content.Intent
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        var REQUEST_OVERLAY_PERMISSIONS = 100
-        if (!Settings.canDrawOverlays(getApplicationContext())) {
-            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            val uri: Uri = Uri.fromParts("package", getPackageName(), null)
-            myIntent.setData(uri)
-            startActivityForResult(myIntent, REQUEST_OVERLAY_PERMISSIONS)
-            return
-        }
-    }
 }
